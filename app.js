4d613c8776d0fd4d63b6804f311b819bd60baff4
Skandar De Anaya <Skandar De Anaya>
2016-10-07 22:35:21 +0900

    added wikipedia

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+var request = require('request');
 var restify = require('restify');
 var builder = require('botbuilder');
 var intents = new builder.IntentDialog();
+
+var wikipediaEnd = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=';
 
 //=========================================================
 // Bot Setup
@@ -35,6 +38,29 @@ intents.matches(/^change name/i, [
     }
 ]);
 
+intents.matches(/^tell me about/i, [
+    function (session) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            builder.Prompts.text(session, 'What is the topic you would like to know about?');
+        }
+    },
+    function (session, results) {
+        request(wikipediaEnd + 'batman', function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var resultJson = JSON.parse(body);
+                var mapped = Object.keys( resultJson.query.pages  ).map(function( uid ){
+                    return (resultJson.query.pages[uid ].uid = uid ) && resultJson.query.pages[uid ];
+                });
+                var article = mapped[0].extract;                
+                var answer = 'Ok... ' + session.userData.name + ', I found this on wikipedia : ' + article;
+                session.send(answer);
+            }
+        });        
+    }
+]);
+
 intents.onDefault([
     function (session, args, next) {
         if (!session.userData.name) {
@@ -50,7 +76,8 @@ intents.onDefault([
 
 bot.dialog('/profile', [
     function (session) {
-        builder.Prompts.text(session, 'Hi! What is your name?');
+        session.send('Hello my name is hellobot, I can help you to search info from the web. Please ask me "tell me about" to help you.');
+        builder.Prompts.text(session, 'First, What is your name?');
     },
     function (session, results) {
         session.userData.name = results.response;
